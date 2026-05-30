d65239b9e3eaaf9b70331c145088e7c81df88be1
tracer-ysyx <tracer@ysyx.org>
2026-05-30 15:29:58 +0800

    > sim RTL ysyx_22040000 张皓然 Linux linux 6.8.0-111-generic #111~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Apr 14 17:13:45 UTC  x86_64 x86_64 x86_64 GNU/Linux  15:29:58 up 35 min,  1 user,  load average: 0.44, 0.59, 0.57

--- npc/csrc/driver.cpp
+++ npc/csrc/driver.cpp
@@ -1,47 +1,16 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <assert.h>
+#include <nvboard.h>
+#include <Vtop.h>
 
-#include "Vtop.h"
-#include "verilated.h"
-#include "verilated_fst_c.h"    // 如果使用 FST 格式
-// #include "verilated_vcd_c.h" // 如果使用 VCD 格式
+static TOP_NAME dut;
 
-#define MAX_SIM_CYCLES 100
+void nvboard_bind_all_pins(TOP_NAME* top);
 
-int main(int argc, char** argv) {
-    VerilatedContext* contextp = new VerilatedContext;
-    contextp->commandArgs(argc, argv);
-    Vtop* top = new Vtop{contextp};
+int main() {
+    nvboard_bind_all_pins(&dut);
+    nvboard_init();
 
-    contextp->traceEverOn(true);              // 启用跟踪
-
-    // 创建波形文件对象
-    VerilatedFstC* tfp = new VerilatedFstC;   // FST 格式
-    // VerilatedVcdC* tfp = new VerilatedVcdC; // VCD 格式
-
-    // 将 top 模块的信号写入波形，跟踪深度为 99
-    top->trace(tfp, 99);
-    tfp->open("wave.fst");                    // 生成 wave.fst
-
-    for (int i = 0; i < MAX_SIM_CYCLES && !contextp->gotFinish(); i++) {
-        int a = rand() & 1;
-        int b = rand() & 1;
-        top->a = a;
-        top->b = b;
-        top->eval();
-
-        // 在每个时间步后 dump 波形
-        tfp->dump(contextp->time());          // 写入当前时间点的波形
-        contextp->timeInc(1);                 // 时间递增
-
-        printf("a = %d, b = %d, f = %d\n", a, b, top->f);
-        assert(top->f == (a ^ b));
+    while(1) {
+    dut.eval();
+    nvboard_update();
     }
-
-    tfp->close();
-    delete tfp;
-    delete top;
-    delete contextp;
-    return 0;
 }
