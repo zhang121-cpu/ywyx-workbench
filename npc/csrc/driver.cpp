641d27a5c97616bcb39f9aa4c779d66c28275111
tracer-ysyx <tracer@ysyx.org>
2026-05-30 16:22:30 +0800

    > sim RTL ysyx_22040000 张皓然 Linux linux 6.8.0-111-generic #111~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Apr 14 17:13:45 UTC  x86_64 x86_64 x86_64 GNU/Linux  16:22:30 up 44 min,  1 user,  load average: 0.49, 0.80, 0.71

--- npc/csrc/driver.cpp
+++ npc/csrc/driver.cpp
@@ -5,12 +5,25 @@ static TOP_NAME dut;
 
 void nvboard_bind_all_pins(TOP_NAME* top);
 
+static void single_cycle() {
+    dut.clk = 0; dut.eval();
+    dut.clk = 1; dut.eval();
+}
+
+static void reset(int n) {
+    dut.rst = 1;
+    while (n -- > 0) single_cycle();
+    dut.rst = 0;
+}
+
 int main() {
     nvboard_bind_all_pins(&dut);
     nvboard_init();
 
+    reset(10);
+
     while(1) {
-    dut.eval();
-    nvboard_update();
+        nvboard_update();
+        single_cycle();
     }
-}
+    }
--- npc/vsrc/top.v
+++ npc/vsrc/top.v
@@ -1,7 +1,25 @@
 module top(
-  input a,
-  input b,
-  output f
+  input clk,
+  input rst,
+  output led[7:0]
 );
-  assign f = a ^ b;
+
+  reg [7:0] count;
+
+  always @(posedge clk ) begin
+    if (rst)
+      count <= 0;
+    else 
+      count <= count + 1;
+  end
+
+  assign led[7] = (count[7])&(count[6])&(count[5]);
+  assign led[6] = (count[7])&(count[6])&(~count[5]);
+  assign led[5] = (count[7])&(~count[6])&(count[5]);
+  assign led[4] = (count[7])&(~count[6])&(~count[5]);
+  assign led[3] = (~count[7])&(count[6])&(count[5]);
+  assign led[2] = (~count[7])&(count[6])&(~count[5]);
+  assign led[1] = (~count[7])&(~count[6])&(count[5]);
+  assign led[0] = (~count[7])&(~count[6])&(~count[5]);
+
 endmodule
