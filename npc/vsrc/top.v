ba713409d73ede6af9e6e618154e5667e1504100
tracer-ysyx <tracer@ysyx.org>
2026-05-30 16:29:42 +0800

    > sim RTL ysyx_22040000 张皓然 Linux linux 6.8.0-111-generic #111~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Apr 14 17:13:45 UTC  x86_64 x86_64 x86_64 GNU/Linux  16:29:42 up 52 min,  1 user,  load average: 0.87, 1.06, 0.89

--- npc/vsrc/top.v
+++ npc/vsrc/top.v
@@ -4,7 +4,7 @@ module top(
   output led[7:0]
 );
 
-  reg [7:0] count;
+  reg [15:0] count;
 
   always @(posedge clk ) begin
     if (rst)
@@ -13,13 +13,13 @@ module top(
       count <= count + 1;
   end
 
-  assign led[7] = (count[7])&(count[6])&(count[5]);
-  assign led[6] = (count[7])&(count[6])&(~count[5]);
-  assign led[5] = (count[7])&(~count[6])&(count[5]);
-  assign led[4] = (count[7])&(~count[6])&(~count[5]);
-  assign led[3] = (~count[7])&(count[6])&(count[5]);
-  assign led[2] = (~count[7])&(count[6])&(~count[5]);
-  assign led[1] = (~count[7])&(~count[6])&(count[5]);
-  assign led[0] = (~count[7])&(~count[6])&(~count[5]);
+  assign led[7] = (count[15])&(count[14])&(count[13]);
+  assign led[6] = (count[15])&(count[14])&(~count[13]);
+  assign led[5] = (count[15])&(~count[14])&(count[13]);
+  assign led[4] = (count[15])&(~count[14])&(~count[13]);
+  assign led[3] = (~count[15])&(count[14])&(count[13]);
+  assign led[2] = (~count[15])&(count[14])&(~count[13]);
+  assign led[1] = (~count[15])&(~count[14])&(count[13]);
+  assign led[0] = (~count[15])&(~count[14])&(~count[13]);
 
 endmodule
