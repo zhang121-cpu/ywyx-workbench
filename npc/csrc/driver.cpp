0e8ffbacbf1af973fae5eb2ad095ed5a954eea5f
tracer-ysyx <tracer@ysyx.org>
2026-06-06 09:43:23 +0800

    > sim RTL ysyx_22040000 张皓然 Linux linux 6.8.0-124-generic #124~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue May 26 21:05:19 UTC  x86_64 x86_64 x86_64 GNU/Linux  09:43:23 up 11 min,  1 user,  load average: 1.10, 1.00, 0.62

--- npc/csrc/driver.cpp
+++ npc/csrc/driver.cpp
@@ -6,8 +6,10 @@ static TOP_NAME dut;
 void nvboard_bind_all_pins(TOP_NAME* top);
 
 static void single_cycle() {
-    //dut.clk = 0; dut.eval();
-    //dut.clk = 1; dut.eval();
+    //dut.clk = 0; 
+    dut.eval();
+    //dut.clk = 1; 
+    dut.eval();
 }
 
 static void reset(int n) {
