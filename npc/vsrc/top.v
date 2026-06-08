0ce801365139eced1667ad434affe8daa91843ab
Zhang Haoran <843321181@qq.com>
2026-06-08 22:46:46 +0800

    2026/6/8

--- npc/vsrc/top.v
+++ npc/vsrc/top.v
@@ -1,7 +1,14 @@
-module top(
-  input a,
-  input b,
-  output f
+module top (
+    input clk,
+    input rst,
+    output [7:0] out
 );
-  assign f = a ^ b;
+
+    Shift_Register shift_register1 (
+        .in(out[4]^out[3]^out[2]^out[0]),
+        .clk(clk),
+        .rst(rst),
+        .out(out)
+    );
+
 endmodule
