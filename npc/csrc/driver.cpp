dfd9b515d5974a1f4a0c7811bc621cd28574f8ee
tracer-ysyx <tracer@ysyx.org>
2026-06-08 22:41:55 +0800

    > sim RTL ysyx_22040000 张皓然 Linux linux 6.8.0-124-generic #124~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue May 26 21:05:19 UTC  x86_64 x86_64 x86_64 GNU/Linux  22:41:55 up 7 min,  1 user,  load average: 1.87, 1.25, 0.70

--- npc/csrc/driver.cpp
+++ npc/csrc/driver.cpp
@@ -25,7 +25,7 @@ int main() {
     reset(10);
 
     while(1) {
-        for (int i = 0; i < 1000; i++) {
+        for (int i = 0; i < 100000; i++) {
             // do nothing, just waste some time
         }
         nvboard_update();
