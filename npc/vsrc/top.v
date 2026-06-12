c208110d8e9c513bd9f00e8b4522f7b209316999
tracer-ysyx <tracer@ysyx.org>
2026-06-13 00:00:48 +0800

    > sim RTL ysyx_22040000 张皓然 Linux linux 6.8.0-124-generic #124~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue May 26 21:05:19 UTC  x86_64 x86_64 x86_64 GNU/Linux  00:00:48 up  1:08,  1 user,  load average: 0.96, 0.61, 0.46

--- npc/vsrc/top.v
+++ npc/vsrc/top.v
@@ -1,14 +1,53 @@
-module top (
+module top(
     input clk,
     input rst,
-    output [7:0] out
+    output VGA_HSYNC,
+    output VGA_VSYNC,
+    output VGA_BLANK_N,
+    output [7:0] VGA_R,
+    output [7:0] VGA_G,
+    output [7:0] VGA_B
 );
 
-    Shift_Register shift_register1 (
-        .in(out[4]^out[3]^out[2]^out[0]),
-        .clk(clk),
-        .rst(rst),
-        .out(out)
+    wire [9:0] h_addr;
+    wire [9:0] v_addr;
+    wire [23:0] vga_data;
+
+    vga_ctrl i0 (
+    .pclk(clk),
+    .reset(rst),
+    .vga_data(vga_data),
+    .h_addr(h_addr),
+    .v_addr(v_addr),
+    .hsync(VGA_HSYNC),
+    .vsync(VGA_VSYNC),
+    .valid(VGA_BLANK_N),
+    .vga_r(VGA_R),
+    .vga_g(VGA_G),
+    .vga_b(VGA_B)
     );
+
+    vmem i1 (
+        .h_addr(h_addr),
+        .v_addr(v_addr[8:0]),
+        .vga_data(vga_data)
+    );
+
+endmodule
+
+
+module vmem(
+    input [9:0] h_addr,
+    input [8:0] v_addr,
+    output [23:0] vga_data
+);
+
+reg [23:0] vga_mem [524287:0];
+
+initial begin
+    $readmemh("resource/picture.hex", vga_mem);
+end
+
+assign vga_data = vga_mem[{h_addr, v_addr}];
 
 endmodule
