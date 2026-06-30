efbb796df4ee5a503823c6da5f4467ea45b6db0b
tracer-ysyx <tracer@ysyx.org>
2026-06-30 13:44:16 +0800

    > compile NEMU ysyx_22040000 张皓然 Linux linux 6.8.0-124-generic #124~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue May 26 21:05:19 UTC  x86_64 x86_64 x86_64 GNU/Linux  13:44:16 up 5 min,  1 user,  load average: 0.79, 0.86, 0.48

--- nemu/src/monitor/sdb/sdb.c
+++ nemu/src/monitor/sdb/sdb.c
@@ -145,27 +145,11 @@ static int cmd_x(char *args){
 
   vaddr_t addr = strtoul(addr_str, NULL, 0);//可自动识别并转换包括0x十六进制在内的各种进制
 
-  // for (int i = 0; i < atoi(num); i++){
-  //   printf("0x%08x: 0x%08x\n", addr + i * 4, vaddr_read(addr + i * 4, 4));
-  // } 
-  // for (int i = 0; i < atoi(num); i++){
-  //   if (i % 4 == 0) {
-  //     printf("0x%08x:", addr + i * 4);
-  //   }
-  //   printf("  0x%08x", vaddr_read(addr + i * 4, 4));
-  //   if (i % 4 == 3) {
-  //     printf("\n");
-  //   }
-  //   if (atoi(num) - 1 == i && i % 4 != 3) {
-  //     printf("\n");
-  //   }
-  // }
-
   for (int i = 0; i < atoi(num); i++){
     if (i % 4 == 0) {
-      printf("0x%08x\t:", addr + i * 4);
+      printf("0x%08x:", addr + i * 4);
     }
-    printf("0x%08x\t", vaddr_read(addr + i * 4, 4));
+    printf("  0x%08x", vaddr_read(addr + i * 4, 4));
     if (i % 4 == 3) {
       printf("\n");
     }
