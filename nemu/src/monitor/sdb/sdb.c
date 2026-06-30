da558cf2b94aa3f5d2ecfc5c76fa7978b2b4c766
tracer-ysyx <tracer@ysyx.org>
2026-06-30 13:49:02 +0800

    > compile NEMU ysyx_22040000 张皓然 Linux linux 6.8.0-124-generic #124~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue May 26 21:05:19 UTC  x86_64 x86_64 x86_64 GNU/Linux  13:49:02 up 10 min,  1 user,  load average: 0.57, 0.73, 0.52

--- nemu/src/monitor/sdb/sdb.c
+++ nemu/src/monitor/sdb/sdb.c
@@ -143,19 +143,13 @@ static int cmd_x(char *args){
     return 0;
   }
 
-  vaddr_t addr = strtoul(addr_str, NULL, 0);//可自动识别并转换包括0x十六进制在内的各种进制
+  vaddr_t addr = strtoul(addr_str, NULL, 0);        //可自动识别并转换包括0x十六进制在内的各种进制
 
   for (int i = 0; i < atoi(num); i++){
-    if (i % 4 == 0) {
-      printf("0x%08x:", addr + i * 4);
-    }
+    if (i % 4 == 0) printf("0x%08x:", addr + i * 4);        //每4条指令重新打印1行地址
     printf("  0x%08x", vaddr_read(addr + i * 4, 4));
-    if (i % 4 == 3) {
-      printf("\n");
-    }
-    if (atoi(num) - 1 == i && i % 4 != 3) {
-      printf("\n");
-    }
+    if (i % 4 == 3) printf("\n");                                             //每4条指令换行                                                                                                    
+    if (atoi(num) - 1 == i && i % 4 != 3) printf("\n");  //最后1行指令不足4条时换行
   }
   return 0;
 }
